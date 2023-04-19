180b4cd6aefa0d5c666404000bb33937772b782e
az87346 <az87346@uga.edu>
2023-04-18 22:31:59 -0400

    Connect firebase and first email user login

--- app/src/main/java/edu/uga/cs/roommateshopping/MainActivity.java
+++ app/src/main/java/edu/uga/cs/roommateshopping/MainActivity.java
@@ -7,10 +7,26 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "RoommateShopping";
+    private FirebaseAuth mAuth;
 
     private Fragment currentFragment;
 
@@ -28,6 +44,45 @@ public class MainActivity extends AppCompatActivity {
             changeFragment(new SplashFragment());
         }
 
+        mAuth = FirebaseAuth.getInstance();
+        String email = "dawg@mail.com";
+        String password = "password";
+
+        mAuth.signInWithEmailAndPassword( email, password )
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+// Sign in success
+                            Log.d( TAG, "signInWithEmail:success" );
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        }
+                        else {
+// If sign in fails
+                            Log.d( TAG, "signInWithEmail:failure", task.getException() );
+                        }
+                    }
+                });
+
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference( "message" );
+//
+//        // Read from the database value for ”message”
+//        myRef.addValueEventListener( new ValueEventListener() {
+//            @Override
+//            public void onDataChange( DataSnapshot dataSnapshot ) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String message = dataSnapshot.getValue( String.class );
+//                textView.setText( message );
+//            }
+
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w( TAG, "Failed to read value.", error.toException() );
+//            }
+//        });
 
     }
 
